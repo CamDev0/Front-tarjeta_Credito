c70a407c0af567237e02df20f93784a721e4fadd
Camilo Barrios <camilobarriosdev@gmail.com>
2023-06-12 22:02:19 -0500

    End crud in web, conected back with front

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -14,13 +14,8 @@ import { CardService } from 'src/app/services/card.service';
 
 export class TarjetaCreditoComponent implements OnInit {
   //Creamos la lista de tarjetas y unos objetos
-  listCard: any[] = [
-    {titular:'Camilo Barrios', number: '122233232323', expiracion:'06/25', cvv:'343'},
-    {titular:'Felipe Lopez', number: '342443545466', expiracion:'01/26', cvv:'231'},
-    {titular:'Marcela Puentes', number: '2123657878766', expiracion:'03/24', cvv:'443'},
-    {titular:'Héctor Cardenas', number: '3232343535353', expiracion:'10/27', cvv:'875'},
-  ];
-
+  listCard: any[] = [];
+  id: number | undefined;
   form: FormGroup;
 
   //En el constructor inicializamos y ponemos las validaciones
@@ -37,37 +32,84 @@ export class TarjetaCreditoComponent implements OnInit {
       this.getCard();
   }
 
+  //Get front -> List
+
   getCard(){
     this._cardService.getListCard().subscribe(data => {
       console.log(data);
+      //Con esta instrucción pusheamos los datos de la BD a la lista del front
+      this.listCard = data;
     }, error => {
       console.log(error);
       
     })
   }
 
-  agregarTarjeta(){
-    console.log(this.form);
+
+  //POST front
+
+  saveTarjeta(){
 
     //Obtenemos los valores que se ingresan en el form
-    const tarjet: any = {
+    const tarjeta: any = {
       titular: this.form.get('titular')?.value,
       number: this.form.get('number')?.value,
       expiracion: this.form.get('expiracion')?.value,
       cvv: this.form.get('cvv')?.value
     }
 
-    //Pusheamos los datos en el listCard, y sacamos un modal que importamos con el toasttr
-    this.listCard.push(tarjet);
-    this.toastr.success('Tarjeta agregada con éxito.', 'Tarjeta Agregada');
-    //Reseteamos el form
-    this.form.reset();
-    console.log(tarjet);
+    if(this.id == undefined)
+    {
+      //Agg nueva tarjeta
+      this._cardService.saveCard(tarjeta).subscribe(data => {
+        this.toastr.success('Tarjeta agregada con éxito.', 'Tarjeta Agregada');
+        this.getCard();
+        this.form.reset();
+      }, error =>{
+        this.toastr.error('Reintente por favor.', 'Error');
+        console.log(error);
+      })
+    }
+    else
+    {
+      //Edit Tarjeta
+      tarjeta.id = this.id;
+      this._cardService.updateCard(this.id, tarjeta).subscribe(data => {
+        this.form.reset();
+        this.id = undefined;
+        this.toastr.success("Tarjeta actualizada con éxito", "Tarjeta Actualizada");
+        this.getCard();
+      }, error =>{
+        this.toastr.error("Ha ocurrido un error", "Error");
+      })
+    }
   }
 
-  deleteCard(index: number){
+  //PUT
+  editCards(tarjeta: any){
+    this.id = tarjeta.id;
+
+    console.log(tarjeta);
+    this.form.patchValue({
+      
+      titular: tarjeta.titular,
+      number: tarjeta.numberCard,
+      expiracion: tarjeta.expiracion,
+      cvv: tarjeta.cvv
+    })
+  }
+
+
+  //DELETE front
+
+  deleteCard(id: number){
     //el .splice le pasamos la posición del index al dar click en borrar y le decimos que elimine solo esa posición
-    this.listCard.splice(index, 1)
-    this.toastr.error("La tarjeta fué eliminada con éxito.", "Tarjeta Eliminada")
+    this._cardService.deleteCard(id).subscribe(data => {
+      this.toastr.error("La tarjeta fué eliminada con éxito.", "Tarjeta Eliminada")
+      this.getCard();
+    }, error => {
+      console.log(error);
+      this.toastr.error("Reintente por favor.", "Error")
+    })
   }
 }
--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -13,9 +13,28 @@ export class CardService {
   //Inyectamos una variable HttpClient
   constructor(private http: HttpClient) { }
 
-  //El observable se una par hacer peticiones Async, retorna un formato JSON 
+  //GET .. El observable se una par hacer peticiones Async, retorna un formato JSON 
   getListCard(): Observable<any>
   {
     return this.http.get(this.myAppUrl + this.myApiUrl);
+  }
+
+  //DELETE
+  deleteCard(id: number): Observable<any>
+  {
+    //En el argumento del delete armamos la ruta url con el id que se va a eliminar
+    return this.http.delete(this.myAppUrl + this.myApiUrl + id)
+  }
+
+  //POST
+  saveCard(tarjeta: any): Observable<any>
+  {
+    return this.http.post(this.myAppUrl + this.myApiUrl, tarjeta);
+  }
+
+  //PUT 
+  updateCard(id: number, tarjeta: any): Observable<any>
+  {
+    return this.http.put(this.myAppUrl + this.myApiUrl + id, tarjeta);
   }
 }
