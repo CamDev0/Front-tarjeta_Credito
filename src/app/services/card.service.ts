2713ff737b8e249f02ffd33ac5d94503e8f48a30
Camilo Barrios <camilobarriosdev@gmail.com>
2023-06-18 14:38:06 -0500

    modificando url de prod

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class CardService {
 
+  private URLprod = "https://fbcreditcard20230618135144.azurewebsites.net/api/card/";
   private myAppUrl = "https://localhost:7042/"; //url del backend
   private myApiUrl = "api/card/" //url del api de los controladores
 
@@ -16,25 +17,30 @@ export class CardService {
   //GET .. El observable se una par hacer peticiones Async, retorna un formato JSON 
   getListCard(): Observable<any>
   {
-    return this.http.get(this.myAppUrl + this.myApiUrl);
+    // return this.http.get(this.myAppUrl + this.myApiUrl);
+    return this.http.get(this.URLprod);
   }
 
   //DELETE
   deleteCard(id: number): Observable<any>
   {
     //En el argumento del delete armamos la ruta url con el id que se va a eliminar
-    return this.http.delete(this.myAppUrl + this.myApiUrl + id)
+
+    // return this.http.delete(this.myAppUrl + this.myApiUrl + id)
+    return this.http.delete(this.URLprod + id)
   }
 
   //POST
   saveCard(tarjeta: any): Observable<any>
   {
-    return this.http.post(this.myAppUrl + this.myApiUrl, tarjeta);
+    // return this.http.post(this.myAppUrl + this.myApiUrl, tarjeta);
+    return this.http.post(this.URLprod, tarjeta);
   }
 
   //PUT 
   updateCard(id: number, tarjeta: any): Observable<any>
   {
-    return this.http.put(this.myAppUrl + this.myApiUrl + id, tarjeta);
+    // return this.http.put(this.myAppUrl + this.myApiUrl + id, tarjeta);
+    return this.http.put(this.URLprod + id, tarjeta);
   }
 }
